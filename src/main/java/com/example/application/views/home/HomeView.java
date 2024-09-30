11dbe4a3b5186f5f9efe86e097ec67118c6d011a
Matthew Wilson <matthew_w@vaadin.com>
2024-09-30 07:34:11 +0300

    Add a combo box

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -2,14 +2,10 @@ package com.example.application.views.home;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
 @PageTitle("Home")
 @Route(value = "")
